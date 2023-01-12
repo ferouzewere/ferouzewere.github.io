c4bd1e709db38546e551adeda924c1e67b0ac29a
ferouze <ferouzewere@gmail.com>
2023-01-12 22:25:14 +0300

    age changes

---
+++ assets/js/age.js
@@ -0,0 +1,10 @@
+window.onload = function () {
+    var birthdate = new Date("11/24/1998");
+    var today = new Date();
+    var age = today.getFullYear() - birthdate.getFullYear();
+    var m = today.getMonth() - birthdate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthdate.getDate())) {
+        age--;
+    }
+    document.getElementById("age").innerHTML = age;
+}
